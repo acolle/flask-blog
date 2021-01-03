60c1566e035215900e52e681f54568267f4366e9
acolle <anthonycolle84@gmail.com>
2021-01-03 19:42:46 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import io
+
+from setuptools import find_packages
+from setuptools import setup
+
+with io.open("README.rst", "rt", encoding="utf8") as f:
+    readme = f.read()
+
+setup(
+    name="flaskr",
+    version="1.0.0",
+    license="BSD",
+    maintainer="Anthony Colle",
+    description="A basic blog app built with Flask.",
+    long_description=readme,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["flask"],
+    extras_require={"test": ["pytest", "coverage"]},
+)
